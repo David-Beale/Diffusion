ec39661e25942ddceb8f2e9782edf81cc85f4039
David B <59053870+David-Beale@users.noreply.github.com>
2021-10-05 08:15:12 +0100

    add buffer mesh to decrease gpu data transfer

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,25 +2,49 @@ import { VertexColors } from "three";
 import { useData } from "./useData";
 
 export default function Main({ length }) {
-  const [meshRef, colorRef, colorArray] = useData({
+  const [
+    meshRef,
+    colorRef,
+    colorArray,
+    bufferMeshRef,
+    bufferColorRef,
+    bufferColorArray,
+  ] = useData({
     length,
   });
 
   return (
-    <instancedMesh
-      ref={meshRef}
-      args={[null, null, length]}
-      frustumCulled={false}
-    >
-      <sphereBufferGeometry args={[1, 16, 16]}>
-        <instancedBufferAttribute
-          ref={colorRef}
-          attachObject={["attributes", "color"]}
-          args={[colorArray, 3]}
-        />
-      </sphereBufferGeometry>
+    <>
+      <instancedMesh
+        ref={meshRef}
+        args={[null, null, length]}
+        frustumCulled={false}
+      >
+        <sphereBufferGeometry args={[1, 16, 16]}>
+          <instancedBufferAttribute
+            ref={colorRef}
+            attachObject={["attributes", "color"]}
+            args={[colorArray, 3]}
+          />
+        </sphereBufferGeometry>
 
-      <meshStandardMaterial attach="material" vertexColors={VertexColors} />
-    </instancedMesh>
+        <meshStandardMaterial attach="material" vertexColors={VertexColors} />
+      </instancedMesh>
+      <instancedMesh
+        ref={bufferMeshRef}
+        args={[null, null, 100]}
+        frustumCulled={false}
+      >
+        <sphereBufferGeometry args={[1, 16, 16]}>
+          <instancedBufferAttribute
+            ref={bufferColorRef}
+            attachObject={["attributes", "color"]}
+            args={[bufferColorArray, 3]}
+          />
+        </sphereBufferGeometry>
+
+        <meshStandardMaterial attach="material" vertexColors={VertexColors} />
+      </instancedMesh>
+    </>
   );
 }
--- src/Components/Main/useData.js
+++ src/Components/Main/useData.js
@@ -12,9 +12,13 @@ const color3 = new Color("blue");
 
 export const useData = ({ length }) => {
   const meshRef = useRef();
+  const bufferMeshRef = useRef();
   const colorRef = useRef();
+  const bufferColorRef = useRef();
   const colorArray = useMemo(() => new Float32Array(length * 3), [length]);
+  const bufferColorArray = useMemo(() => new Float32Array(300), []);
   const index = useRef(1);
+  const bufferIndex = useRef(0);
   const w = useRef(new Walker({ start: false, boundingDist: 4 }));
   const boundingDist = useRef(4);
   const tree = useRef(new Octree());
@@ -35,7 +39,22 @@ export const useData = ({ length }) => {
   }, [colorArray, length]);
 
   useFrame(() => {
-    if (index.current === length || boundingDist.current > 100) return;
+    if (index.current >= length || boundingDist.current > 100) return;
+    if (bufferIndex.current === 100) {
+      for (let i = index.current * 16; i < index.current * 16 + 1600; i++) {
+        meshRef.current.instanceMatrix.array[i] =
+          bufferMeshRef.current.instanceMatrix.array[i - index.current * 16];
+      }
+      for (let i = index.current * 3; i < index.current * 3 + 300; i++) {
+        colorArray[i] = bufferColorArray[i - index.current * 3];
+      }
+      colorRef.current.needsUpdate = true;
+      meshRef.current.instanceMatrix.needsUpdate = true;
+      index.current += 100;
+      meshRef.current.count = index.current;
+      bufferIndex.current = 0;
+    }
+
     while (!w.current.stuck) {
       w.current.walk();
       range.current.set(w.current);
@@ -49,23 +68,33 @@ export const useData = ({ length }) => {
     scratchObject3D.position.set(x, y, z);
     scratchObject3D.scale.set(1, 1, 1);
     scratchObject3D.updateMatrix();
-    meshRef.current.setMatrixAt(index.current, scratchObject3D.matrix);
+    bufferMeshRef.current.setMatrixAt(
+      bufferIndex.current,
+      scratchObject3D.matrix
+    );
 
     if (wDistFromOrigin < 50) {
       scratchColor.lerpColors(color1, color2, wDistFromOrigin / 50);
     } else {
       scratchColor.lerpColors(color2, color3, (wDistFromOrigin - 50) / 50);
     }
-    scratchColor.toArray(colorArray, index.current * 3);
+    scratchColor.toArray(bufferColorArray, bufferIndex.current * 3);
 
     tree.current.insert(w.current.clone());
     w.current.reset(boundingDist.current);
-    index.current++;
+    bufferIndex.current++;
 
-    colorRef.current.needsUpdate = true;
-    meshRef.current.instanceMatrix.needsUpdate = true;
-    meshRef.current.count = index.current;
+    bufferColorRef.current.needsUpdate = true;
+    bufferMeshRef.current.instanceMatrix.needsUpdate = true;
+    bufferMeshRef.current.count = bufferIndex.current;
   });
 
-  return [meshRef, colorRef, colorArray];
+  return [
+    meshRef,
+    colorRef,
+    colorArray,
+    bufferMeshRef,
+    bufferColorRef,
+    bufferColorArray,
+  ];
 };
