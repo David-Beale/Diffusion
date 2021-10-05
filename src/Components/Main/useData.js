2b3415712bcf834d48cfe9acf74a28c13a0ef46a
David B <59053870+David-Beale@users.noreply.github.com>
2021-10-05 08:22:17 +0100

    refactor

--- src/Components/Main/useData.js
+++ src/Components/Main/useData.js
@@ -1,5 +1,5 @@
 import { useFrame } from "@react-three/fiber";
-import { useEffect, useMemo, useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef } from "react";
 import { Color, Object3D } from "three";
 import Octree from "./Octree/Octree";
 import Sphere from "./Octree/Sphere";
@@ -38,23 +38,22 @@ export const useData = ({ length }) => {
     tree.current.insert(new Walker({ start: true }));
   }, [colorArray, length]);
 
-  useFrame(() => {
-    if (index.current >= length || boundingDist.current > 100) return;
-    if (bufferIndex.current === 100) {
-      for (let i = index.current * 16; i < index.current * 16 + 1600; i++) {
-        meshRef.current.instanceMatrix.array[i] =
-          bufferMeshRef.current.instanceMatrix.array[i - index.current * 16];
-      }
-      for (let i = index.current * 3; i < index.current * 3 + 300; i++) {
-        colorArray[i] = bufferColorArray[i - index.current * 3];
-      }
-      colorRef.current.needsUpdate = true;
-      meshRef.current.instanceMatrix.needsUpdate = true;
-      index.current += 100;
-      meshRef.current.count = index.current;
-      bufferIndex.current = 0;
+  const transferBufferData = useCallback(() => {
+    for (let i = index.current * 16; i < index.current * 16 + 1600; i++) {
+      meshRef.current.instanceMatrix.array[i] =
+        bufferMeshRef.current.instanceMatrix.array[i - index.current * 16];
     }
+    for (let i = index.current * 3; i < index.current * 3 + 300; i++) {
+      colorArray[i] = bufferColorArray[i - index.current * 3];
+    }
+    colorRef.current.needsUpdate = true;
+    meshRef.current.instanceMatrix.needsUpdate = true;
+    index.current += 100;
+    meshRef.current.count = index.current;
+    bufferIndex.current = 0;
+  }, [bufferColorArray, colorArray]);
 
+  const runWalker = useCallback(() => {
     while (!w.current.stuck) {
       w.current.walk();
       range.current.set(w.current);
@@ -64,6 +63,14 @@ export const useData = ({ length }) => {
     if (wDistFromOrigin > boundingDist.current) {
       boundingDist.current = wDistFromOrigin + 4;
     }
+    if (wDistFromOrigin < 50) {
+      scratchColor.lerpColors(color1, color2, wDistFromOrigin / 50);
+    } else {
+      scratchColor.lerpColors(color2, color3, (wDistFromOrigin - 50) / 50);
+    }
+  }, []);
+
+  const updateBufferMesh = useCallback(() => {
     const { x, y, z } = w.current.pos;
     scratchObject3D.position.set(x, y, z);
     scratchObject3D.scale.set(1, 1, 1);
@@ -73,20 +80,26 @@ export const useData = ({ length }) => {
       scratchObject3D.matrix
     );
 
-    if (wDistFromOrigin < 50) {
-      scratchColor.lerpColors(color1, color2, wDistFromOrigin / 50);
-    } else {
-      scratchColor.lerpColors(color2, color3, (wDistFromOrigin - 50) / 50);
-    }
     scratchColor.toArray(bufferColorArray, bufferIndex.current * 3);
-
-    tree.current.insert(w.current.clone());
-    w.current.reset(boundingDist.current);
     bufferIndex.current++;
 
     bufferColorRef.current.needsUpdate = true;
     bufferMeshRef.current.instanceMatrix.needsUpdate = true;
     bufferMeshRef.current.count = bufferIndex.current;
+  }, [bufferColorArray]);
+
+  const updateOctree = useCallback(() => {
+    tree.current.insert(w.current.clone());
+    w.current.reset(boundingDist.current);
+  }, []);
+
+  useFrame(() => {
+    if (index.current >= length || boundingDist.current > 100) return;
+    if (bufferIndex.current === 100) transferBufferData();
+
+    runWalker();
+    updateBufferMesh();
+    updateOctree();
   });
 
   return [
